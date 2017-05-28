8dd9248e5dd05677b555301bf493976868bf577b
Ryojin <ryojin@gmail.com>
2017-05-28 23:23:13 +1000

    yet more opcodes implemented/fixed fixed relative address calculation implemented basic framebuffer display more refactoring

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>
-
+#include <windows.h>
 
 uint8_t program[] = {0xFE, 0x01, 0x09, 0xA6, 0x00, 0x27, 0x6F, 0xB7, 0x01, 0x0B, 0xC6, 0x30, 0x10, 0xB7, 0x01, 0x0C,
 0x27, 0x12, 0xF6, 0x01, 0x0C, 0xB6, 0x01, 0x0B, 0xBD, 0x00, 0x2A, 0xBD, 0x00, 0x55, 0x7C, 0x01,
@@ -72,8 +72,9 @@ void do_step(registers_t *reg, uint8_t *mem)
     int32_t temp = -1;
     
     //opcode uses accumulator and memory
-    if (reg->IR & 0x80)
+    if ((reg->IR & 0x80) == 0x80)
     {
+        
         //Select accumulator
         uint8_t *Acc = (reg->IR & 0x40) ? &reg->B : &reg->A;
         
@@ -110,7 +111,7 @@ void do_step(registers_t *reg, uint8_t *mem)
         switch(reg->IR & 0x0F)
         {
 
-            //SUB - Subtract from accumulator
+            //SUB - Subtract memory from accumulator
             case(0x00):
             {
                 uint8_t tmp = (*Acc) + TWO_COMP(mem[addr]);
@@ -122,7 +123,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
             
-            //CMP - Compare to accumulator
+            //CMP - Compare memory to accumulator
             case(0x01):
             {
                 uint8_t tmp = *Acc + TWO_COMP(mem[addr]);
@@ -133,7 +134,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
             
-            //SBC - Subtract from accumulator, with carry
+            //SBC - Subtract memory from accumulator, with carry
             case(0x02):
             {
                 uint8_t tmp = (*Acc) + TWO_COMP(mem[addr] + reg->CC.C);
@@ -188,7 +189,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
             
-            //EOR - Logical XOR against accumulator
+            //EOR - Logical XOR, memory against accumulator
             case(0x08):
             {
                 uint8_t tmp = (*Acc) ^ mem[addr];
@@ -199,7 +200,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
 
-            //ADC - Add to accumulator, with carry
+            //ADC - Add memory to accumulator, with carry
             case(0x09):
             {
                 uint8_t tmp = (*Acc) + mem[addr] + reg->CC.C;
@@ -214,7 +215,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
 
-            //ORA - Logical inclusive-OR against accumulator
+            //ORA - Logical inclusive-OR, memory against accumulator
             case(0x0A):
             {
                 uint8_t tmp = (*Acc) | mem[addr];
@@ -225,7 +226,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
 
-            //ADD - Add to accumulator
+            //ADD - Add memory to accumulator
             case(0x0B):
             {
                 uint8_t tmp = (*Acc) + mem[addr];
@@ -240,7 +241,7 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
             
-            //CPX - Compare index register
+            //CPX - Compare index register to memory
             case(0x0C):
             {
                 uint16_t tmp = reg->X - ((mem[addr] << 8) + mem[addr + 1]);
@@ -259,13 +260,26 @@ void do_step(registers_t *reg, uint8_t *mem)
                 break;
             }
             
-            //BSR - Branch to subroutine
+            //BSR/JSR - Branch to subroutine
             case(0x0D):
-            
+                //8D - BSR
+                //AD - JSR (index)
+                //BD - JSR (extended)
+                
+                STACK_PUSH(reg->PC & 0xFF);
+                STACK_PUSH((reg->PC >> 8) & 0xFF);
+                
+                if (reg->IR == 0x8D)    //BSR - relative branch
+                    reg->PC += mem[addr];
+                else                    //JSR - absolute jump
+                    reg->PC = addr;
+                
+                break;
+                
             //LDS/LDX - Load stack pointer/index register
             case(0x0E):
             {
-                uint16_t *tmp = (reg->IR & 0x40) ? &reg->SP : &reg->X;
+                uint16_t *tmp = (reg->IR & 0x40) ? &reg->X : &reg->SP;
                 *tmp = ((mem[addr] << 8) + mem[addr + 1]);
 
                 reg->CC.N = (*tmp & 0x8000) != 0; //bit 15 == 1
@@ -279,7 +293,7 @@ void do_step(registers_t *reg, uint8_t *mem)
             //STS/STX - Store stack pointer/index register
             case(0x0F):
             {
-                uint16_t *tmp = (reg->IR & 0x40) ? &reg->SP : &reg->X;
+                uint16_t *tmp = (reg->IR & 0x40) ? &reg->X : &reg->SP;
                 mem[addr] = ((*tmp >> 8) & 0xFF);
                 mem[addr + 1] = (*tmp & 0xFF);
 
@@ -292,15 +306,130 @@ void do_step(registers_t *reg, uint8_t *mem)
             }           
         }
     }
-    else
+    else if ((reg->IR & 0xF0) == 0x20)    //Branch instruction
     {
-
-
-
-    
         
+        uint8_t tmp = mem[reg->PC + 1];
+        
+        int16_t addr_rel = mem[reg->PC + 1];
+        
+        if (tmp & 0x80)
+        {
+            addr_rel = -1 * (TWO_COMP(tmp));
+        }
+        
+        //addr_rel = (reg->PC & 0x00FF) + mem[reg->PC + 1];
+        
+        
+        
+        reg->PC += 2;
         switch(reg->IR)
         {
+            //BRA - Branch always
+            case(0x20):     
+                reg->PC += addr_rel;
+                break;
+                
+            //BHI - Branch if higher
+            case(0x22):
+                if((reg->CC.C + reg->CC.Z) == 0)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BLS - Branch if lower or same
+            case(0x23):
+                if((reg->CC.C + reg->CC.Z) == 1)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BCC - Branch if carry is clear
+            case(0x24):
+                if(reg->CC.C == 0)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BCS - Branch if carry is set
+            case(0x25):
+                if(reg->CC.C == 1)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BNE - Branch if not zero
+            case(0x26):
+                if(reg->CC.Z == 0)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BEQ - Branch if equals zero
+            case(0x27):
+                if(reg->CC.Z == 1)
+                    reg->PC = addr_rel;
+                break;
+                
+            //BVC - Branch if overflow clear
+            case(0x28):
+                if(reg->CC.V == 0)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BVS - Branch if overflow set
+            case(0x29):
+                if(reg->CC.V == 1)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BPL - Branch if plus
+            case(0x2A):
+                if(reg->CC.N == 0)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BMI - Branch if minus
+            case(0x2B):
+                if(reg->CC.N == 1)
+                    reg->PC += addr_rel;
+                break;
+                
+            //BGE - Branch if greater or equal to zero
+            case(0x2C):
+                if((reg->CC.N ^ reg->CC.V) == 0)
+                    reg->PC = addr_rel;
+                break;
+                
+            //BLT - Branch if less than zero
+            case(0x2D):
+                if((reg->CC.N ^ reg->CC.V) == 1)
+                    reg->PC = addr_rel;
+                break;
+                
+            //BGT - Branch if greater than zero
+            case(0x2E):
+                if((reg->CC.Z + (reg->CC.N ^ reg->CC.V)) == 0)
+                    reg->PC = addr_rel;
+                break;
+                
+            //BLE - Branch if less or equal to zero
+            case(0x2F):
+                if((reg->CC.Z + (reg->CC.N ^ reg->CC.V)) == 1)
+                    reg->PC = addr_rel;
+                break;
+        }
+    }
+    else
+    {
+        
+
+        switch(reg->IR)
+        {
+            case(0x5A):     //DECB - Decrement B
+            {
+                reg->B--;
+                reg->CC.N = (reg->B & 128) != 0;    //Set negative flag
+                reg->CC.Z = (reg->B == 0);            //Set zero flag
+
+                reg->PC++;
+                break;
+            }
 
             case(0x10):     //SBA
             {
@@ -332,6 +461,13 @@ void do_step(registers_t *reg, uint8_t *mem)
                 reg->PC++;
                 break;
             }
+            case(0x37):     //PSHB - B -> stack
+            {
+                STACK_PUSH(reg->B);
+                reg->PC++;
+                break;
+            }
+
             case(0x4C):     //INCA - Implied
             {
                 reg->A++;
@@ -344,162 +480,43 @@ void do_step(registers_t *reg, uint8_t *mem)
                 reg->PC++;
                 break;
             }
+            case(0x33):     //PULB - Implied
+            {
+                reg->B = STACK_POP();
+                reg->PC++;
+                break;
+            }
             case(0x7C):     //INC - extended
             {
                 mem[ADDR_EXT()]++;
                 reg->PC += 3;
                 break;
             }
+            case(0x7F):     //CLR - extended
+            {
+                mem[ADDR_EXT()] = 0;
+                reg->PC += 3;
+                break;
+            }
 
-            /*
-             * Branch if
-             */
-            
-            //BRA - Branch always (Direct)
-            case(0x20):     
-                reg->PC = ADDR_DIR();
-                break;
-                
-            //BHI - Branch if higher (Direct)
-            case(0x22):
-                if((reg->CC.C + reg->CC.Z) == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BLS - Branch if lower or same (Direct)
-            case(0x23):
-                if((reg->CC.C + reg->CC.Z) == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BCC - Branch if carry is clear (Direct)
-            case(0x24):
-                if(reg->CC.C == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BCS - Branch if carry is set (Direct)
-            case(0x25):
-                if(reg->CC.C == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BNE - Branch if not zero (Direct)
-            case(0x26):
-                if(reg->CC.Z == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BEQ - Branch if equals zero (Direct)
-            case(0x27):
-                if(reg->CC.Z == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BVC - Branch if overflow clear (Direct)
-            case(0x28):
-                if(reg->CC.V == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BVS - Branch if overflow set (Direct)
-            case(0x29):
-                if(reg->CC.V == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BPL - Branch if plus (Direct)
-            case(0x2A):
-                if(reg->CC.N == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BMI - Branch if minus (Direct)
-            case(0x2B):
-                if(reg->CC.N == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BGE - Branch if greater or equal to zero (Direct)
-            case(0x2C):
-                if((reg->CC.N ^ reg->CC.V) == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BLT - Branch if less than zero (Direct)
-            case(0x2D):
-                if((reg->CC.N ^ reg->CC.V) == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BGT - Branch if greater than zero (Direct)
-            case(0x2E):
-                if((reg->CC.Z + (reg->CC.N ^ reg->CC.V)) == 0)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-                
-            //BLE - Branch if less or equal to zero (Direct)
-            case(0x2F):
-                if((reg->CC.Z + (reg->CC.N ^ reg->CC.V)) == 1)
-                    reg->PC = ADDR_DIR();
-                else
-                    reg->PC += 2;
-                break;
-            
+ 
             /*
              * Subroutine and interrupts
              */
              
-            //BSR - Branch to subroutine
-            case(0x8D):
-                break;
-            
             //JMP - Jump (index)
             case(0x6E):
                 break;
             
             //JMP - Jump (extended)
             case(0x7E):
+                reg->PC++;
                 reg->PC = mem[ADDR_EXT()];
                 break;
             
-            //JSR - Jump to subroutine (index)
-            case(0xAD):
-                break;
+
             
-            //JSR - Jump to subroutine (extended)
-            case(0xBD):
-                STACK_PUSH(reg->PC & 0xFF);
-                STACK_PUSH((reg->PC >> 8) & 0xFF);
-                reg->PC = (mem[reg->PC + 1] << 8) + mem[reg->PC + 2];
-                break;
-            
+           
             //NOP - No operation (Implied)
             case(0x01):
                 reg->PC++;
@@ -528,6 +545,7 @@ void do_step(registers_t *reg, uint8_t *mem)
             /* 
              * Index register and stack pointer 
              */
+
 
 
             //TXS - Index reg -> stack pointer (Implied)
@@ -663,12 +681,63 @@ void registers_init(registers_t *regs)
 
 }
 
+HANDLE consoleHandle;           //Console handle
+
+
+void setCursorPosition(int x, int y)
+{
+    //Flush stdout buffer
+    fflush(stdout);
+    
+    //Pack cursor coordinates into windows-friendly format
+    COORD coord = { (SHORT)x, (SHORT)y };
+    SetConsoleCursorPosition(consoleHandle, coord);
+}
+
+void cls()
+{
+    COORD coordScreen = {0, 0};
+    DWORD cCharsWritten;
+    CONSOLE_SCREEN_BUFFER_INFO bufferInfo;
+
+    /* get the number of character cells in the current buffer */ 
+    GetConsoleScreenBufferInfo(consoleHandle, &bufferInfo);
+    int ConsoleSize = bufferInfo.dwSize.X * bufferInfo.dwSize.Y;
+
+    /* fill the entire screen with blanks */ 
+    FillConsoleOutputCharacter(consoleHandle, (TCHAR)' ', ConsoleSize, coordScreen, &cCharsWritten);
+
+    /* now set the buffer's attributes accordingly */ 
+    FillConsoleOutputAttribute(consoleHandle, bufferInfo.wAttributes, ConsoleSize, coordScreen, &cCharsWritten);
+
+    setCursorPosition(0, 0);
+}
+
+void printScreen(uint8_t *mem)
+{
+    //cls();
+    setCursorPosition(0,0);
+    
+    //54x20
+    for (int y = 0; y < 20; y++)
+    {
+        for (int x = 0; x < 54; x++)
+        {
+            printf("%c", mem[0xFB00 + (y * 54) +x]);
+        }
+        printf("\n");
+    }
+    
+}
 
 
 int main(int argc, char **argv)
 {
+    
+    consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     int stepping = 0;
-    int breakpoint = 0x03;
+    int breakpoint = -1;
+    //breakpoint = 0x1E;
 	registers_t reg;
     uint8_t mem[0x10000] = {0};
     registers_init(&reg);
@@ -680,13 +749,18 @@ int main(int argc, char **argv)
     {
         //Fetch instruction
         reg.IR = mem[reg.PC];
-        
+
         //exit when we reach a null opcode
         if (reg.IR == 0x00)
+        {
+            printf("Reached null opcode, halting\n");
             break;
+        }
+
         
         do_step(&reg, mem);
-        printf("A:0x%02X B:0x%02X PC:0x%04X X:0x%04X SP:0x%04X IR:0x%02X    H:%d I:%d N:%d Z:%d V:%d C:%d\n", reg.A, reg.B, reg.PC, reg.X, reg.SP, reg.IR, reg.CC.H, reg.CC.I, reg.CC.N, reg.CC.Z, reg.CC.V, reg.CC.C);
+        printScreen(mem);
+        //printf("A:0x%02X B:0x%02X PC:0x%04X X:0x%04X SP:0x%04X IR:0x%02X    H:%d I:%d N:%d Z:%d V:%d C:%d\n", reg.A, reg.B, reg.PC, reg.X, reg.SP, reg.IR, reg.CC.H, reg.CC.I, reg.CC.N, reg.CC.Z, reg.CC.V, reg.CC.C);                
         
         if (breakpoint > 0 && !stepping && reg.PC == breakpoint)
         {
@@ -695,7 +769,10 @@ int main(int argc, char **argv)
         }
             
         if (stepping)
+        {
             getch();
+            //stepping = false;
+        }
     }
 
     
